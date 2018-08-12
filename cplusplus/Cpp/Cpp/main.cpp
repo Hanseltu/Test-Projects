acb42f0f74c4a9ed00217fdf0c94ac3533a4adcf
Hanseltu <tuhaoxin6@gmail.com>
2018-08-12 14:21:30 +0800

    master

--- cplusplus/Cpp/Cpp/main.cpp
+++ cplusplus/Cpp/Cpp/main.cpp
@@ -7,18 +7,67 @@
 //
 
 #include <iostream>
+#include <vector>
 
-const double Inch_per_feet = 12.0L;
-
+//源程序
 int main(int argc, const char * argv[]) {
-    // insert code here...
+   
     using namespace std;
-    cout << "Please enter your height:___ \b\b\b\b\b";
-    double ht_int;
-    cin >> ht_int;
     
-    double ht_feet = ht_int/Inch_per_feet;
-    cout << "Your height is : " << ht_feet << "feets \n";
-    std::cout << "Hello, World!\n";
+    cout << "Please enter the Nu.1 \n";
+    int nu1;
+    cin >> nu1;
+    //cout << nu1;1
+    
+    vector<int> res;
+    vector<int> output;
+    vector<int> ret;
+    
+    cout << "Please enter the Nu.2 \n";
+    
+    int a = 0,b = 0;
+    for (int i=0;i<nu1;i++)
+    {
+        cin >> a;
+        res.push_back(a);
+        //cout << res[i];
+    }
+    
+    cout << "Please enter the Nu.3 \n";
+    int count;
+    cin >> count;
+    
+    cout << "Please enter the Nu.4 \n";
+    for (int i=0;i<count;i++)
+    {
+        cin >> b;
+        output.push_back(b);
+        //cout << res[i];
+    }    //cout << count;
+    
+    
+    
+    for (int i = 0; i< output.size();i++)
+    {
+        int total = 0 ;
+        for (int j = 0; j<res.size();j++){
+            int cn = output[i];
+            total += res[j];
+            if (cn > total){
+                continue;
+            }
+            if (cn <= total) {
+                ret.push_back(j);
+                break;
+            }
+        }
+            
+    }
+    
+    cout << "The result is \n";
+    for (int i=0;i<ret.size();i++){
+        cout << ret[i]+1 << endl;
+    }
+    
     return 0;
 }
