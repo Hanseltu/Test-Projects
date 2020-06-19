b18f18e99c11b1ec507215889cd63cf8bbec2645
Hosea <tuhaoxin6@gmail.com>
2020-06-19 21:21:24 +0800

    Update test-new.cc

--- cplusplus/test-new.cc
+++ cplusplus/test-new.cc
@@ -1,10 +1,21 @@
 void expand_all_functions () {
-        graph* g = build_callgraph ();
-        ipa_perform_analysis (g);
-        function* cfun;
-        FOR_EACH_FUNCTION (g, cfun) {
-                cfun->expand_ipa ();
-                cfun->expand_gimple ();
-                cfun->expand_rtl ();
-        }
+    graph* g = build_callgraph ();
+    ipa_perform_analysis (g);
+    function* cfun;
+    working_set ws;
+
+    FOR_EACH_FUNCTION (g, cfun) {
+        cfun->expand_ipa ();
+    }
+
+    ws.spawn_threads (expand_gimple):
+
+    FOR_EACH_FUNCTION (g, cfun) {
+        ws.insert_work (cfun);
+    }
+    ws.join()
+
+    FOR_EACH_FUNCTION (g, cfun) {
+        cfun->expand_rtl ();
+    }
 }
